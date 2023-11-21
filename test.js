20e52bbfd135fe884f3f3d5334b444bf545c6e82
RicardoRobledo <ricardoprogrammer44@gmail.com>
2023-11-21 15:21:56 -0600

    test.js agregado

---
+++ test.js
@@ -0,0 +1,23 @@
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./app'); 
+chai.use(chaiHttp);
+
+const server = app.listen(3000);
+
+after(() => {
+  server.close();
+});
+
+describe('GET /', () => {
+  it('debería devolver ¡Hola, mundo!', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res.text).to.equal('¡Hola, mundo!');
+        done();
+      });
+  });
+});
